b2f76d00ecabbdf6c48b41d4bb9a01782f9ba3a3
James Dernie <jamesdernie@gmail.com>
2018-04-27 11:34:07 +0800

    Update

--- Epsy/settings.py
+++ Epsy/settings.py
@@ -85,7 +85,6 @@ TEMPLATES = [
 ]
 
 
-
 WSGI_APPLICATION = 'Epsy.wsgi.application'
 
 
@@ -148,12 +147,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Auth
 
